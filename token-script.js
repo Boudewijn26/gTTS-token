37b3b0af416a9b268dbc1a7ce41f9de4b29d4a31
Boudewijn van Groos <boudewijn@vangroos.nl>
2016-06-05 20:31:51 +0200

    Updated the token-script to the latest version

--- token-script.js
+++ token-script.js
@@ -1,62 +1,54 @@
-// Token calculating functions found inline on translate.google.com
-
-var cM = function(a) {
-            return function() {
-                return a
-            }
-        };
-var of = "=";
-var dM = function(a, b) {
+var xr = function(a) {
+    return function() {
+        return a
+    }
+};
+var yr = function(a, b) {
     for (var c = 0; c < b.length - 2; c += 3) {
-        var d = b.charAt(c + 2),
-            d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
-            d = b.charAt(c + 1) == Tb ? a >>> d : a << d;
-        a = b.charAt(c) == Tb ? a + d & 4294967295 : a ^ d
+        var d = b.charAt(c + 2)
+          , d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d)
+          , d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
+        a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
     }
     return a
 };
-
-var eM = null;
-var cb = 0;
-var k = "";
-var Vb = "+-a^+6";
-var Ub = "+-3^+b+-f";
-var t = "a";
-var Tb = "+";
-var dd = ".";
-
-fM = function(a) {
+var zr = null;
+var Ar = function(a) {
     var b;
-    if (null === eM) {
-        var c = cM(String.fromCharCode(84)); // char 84 is T
-        b = cM(String.fromCharCode(75)); // char 75 is K
-        c = [c(), c()];
-        c[1] = b();
-        // So basically we're getting window.TKK
-        eM = Number(window[c.join(b())]) || 0
+    if (null  !== zr)
+        b = zr;
+    else {
+        b = xr(String.fromCharCode(84));
+        var c = xr(String.fromCharCode(75));
+        b = [b(), b()];
+        b[1] = c();
+        b = (zr = window[b.join(c())] || "") || ""
     }
-    b = eM;
-
-    // All this does is convert turn d into the byte values of the utf-8 representation of a
-    var d = cM(String.fromCharCode(116)),
-        c = cM(String.fromCharCode(107)),
-        d = [d(), d()];
+    var d = xr(String.fromCharCode(116))
+      , c = xr(String.fromCharCode(107))
+      , d = [d(), d()];
     d[1] = c();
-    for (var c = cb + d.join(k) +
-            of, d = [], e = 0, f = 0; f < a.length; f++) {
-        var g = a.charCodeAt(f);
-
-        128 > g ? d[e++] = g : (2048 > g ? d[e++] = g >> 6 | 192 : (55296 == (g & 64512) && f + 1 < a.length && 56320 == (a.charCodeAt(f + 1) & 64512) ? (g = 65536 + ((g & 1023) << 10) + (a.charCodeAt(++f) & 1023), d[e++] = g >> 18 | 240, d[e++] = g >> 12 & 63 | 128) : d[e++] = g >> 12 | 224, d[e++] = g >> 6 & 63 | 128), d[e++] = g & 63 | 128)
+    c = "&" + d.join("") + 
+    "=";
+    d = b.split(".");
+    b = Number(d[0]) || 0;
+    for (var e = [], f = 0, g = 0; g < a.length; g++) {
+        var l = a.charCodeAt(g);
+        128 > l ? e[f++] = l : (2048 > l ? e[f++] = l >> 6 | 192 : (55296 == (l & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (l = 65536 + ((l & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+        e[f++] = l >> 18 | 240,
+        e[f++] = l >> 12 & 63 | 128) : e[f++] = l >> 12 | 224,
+        e[f++] = l >> 6 & 63 | 128),
+        e[f++] = l & 63 | 128)
     }
-    // So now utf8(d) == a
-
-
-    a = b || 0;
-    for (e = 0; e < d.length; e++) a += d[e], a = dM(a, Vb);
-    a = dM(a, Ub);
+    a = b;
+    for (f = 0; f < e.length; f++)
+        a += e[f],
+        a = yr(a, "+-a^+6");
+    a = yr(a, "+-3^+b+-f");
+    a ^= Number(d[1]) || 0;
     0 > a && (a = (a & 2147483647) + 2147483648);
     a %= 1E6;
-    return a.toString() + dd + (a ^ b)
-};
-
-console.log(fM("Hello person"));
+    return c + (a.toString() + "." + (a ^ b))
+}
+;
+Ar("test");
